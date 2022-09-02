521b10609257dfd88e92cd07ec05024c1c16fff3
Sidney François <sidney.fvm.ps@outlook.com>
2022-09-02 18:02:27 -0300

    inserindo imagens

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -4,11 +4,10 @@ import "./item.css";
 
 export function Item({ item }) {
   const { addCartItem } = useCart();
-  const img = `src/assets/${item.image}.jpg`;
 
   return (
     <div className="item-container">
-      <img src={img} className="imagem-roupa" />
+      <img src={item.image} className="imagem-roupa" />
       <div className="titulo">{item.name}</div>
       <div>{item.price}</div>
       <button onClick={() => addCartItem(item)}>Adicionar ao carrinho</button>
--- src/components/ListItems/index.jsx
+++ src/components/ListItems/index.jsx
@@ -3,6 +3,16 @@ import { useCart } from "../../contexts/CartContext";
 import { Item } from "../Item";
 import "./listItems.css";
 
+import blazer from "../../assets/blazer.jpg";
+import blazerCreme from "../../assets/blazerCreme.jpg";
+import bolsaCouro from "../../assets/bolsaCouro.jpg";
+import bolsaFendi from "../../assets/bolsaFendi.jpg";
+import bolsaVermelha from "../../assets/bolsaVermelha.jpg";
+import jaqueta from "../../assets/jaqueta.jpg";
+import tenis from "../../assets/tenis.jpg";
+import vestidoBranco from "../../assets/vestidoBranco.jpg";
+import vestidoFlorido from "../../assets/vestidoFlorido.jpg";
+
 export function ListItems() {
   const [items, setItems] = useState([
     {
@@ -10,45 +20,45 @@ export function ListItems() {
       name: "Blazer Branco",
       price: "179,90",
       quantity: 0,
-      image: "blazer",
+      image: blazer,
     },
     {
       id: 2,
       name: "Bolsa de Couro",
       price: "190,90",
       quantity: 0,
-      image: "bolsaCouro",
+      image: bolsaCouro,
     },
     {
       id: 3,
       name: "Bolsa Fendi",
       price: "3.199,90",
       quantity: 0,
-      image: "bolsaFendi",
+      image: bolsaFendi,
     },
-    { id: 4, name: "Blazer", price: "179,90", quantity: 0, image: "blazer" },
+    { id: 4, name: "Blazer", price: "179,90", quantity: 0, image: blazerCreme },
     {
       id: 5,
       name: "Bolsa",
       price: "190,90",
       quantity: 0,
-      image: "bolsaVermelha",
+      image: bolsaVermelha,
     },
     {
       id: 6,
       name: "Vestido",
       price: "3.199,90",
       quantity: 0,
-      image: "vestidoBranco",
+      image: vestidoBranco,
     },
-    { id: 7, name: "Tênis", price: "179,90", quantity: 0, image: "tenis" },
-    { id: 8, name: "Jaqueta", price: "190,90", quantity: 0, image: "jaqueta" },
+    { id: 7, name: "Tênis", price: "179,90", quantity: 0, image: tenis },
+    { id: 8, name: "Jaqueta", price: "190,90", quantity: 0, image: jaqueta },
     {
       id: 9,
       name: "Vestido Florido",
       price: "3.199,90",
       quantity: 0,
-      image: "vestidoFlorido",
+      image: vestidoFlorido,
     },
   ]);
 
